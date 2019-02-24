78135bb747eff949b7229abd755bebbbf6885a8f
Cloud User <ec2-user@ip-172-31-16-90.us-east-2.compute.internal>
2019-02-24 07:42:31 +0000

    added

--- Integers.java
+++ Integers.java
@@ -3,22 +3,9 @@ class Integers {
     int c; //declaring a variable
  
   /* Using for loop to repeat instruction execution */
- 
-    for (c = 1; c <= 12; c++) {
+
+    for (c = 1; c <= 13; c++) {
       System.out.println(c);
     }
   }
-<<<<<<< HEAD
-}class Integers {
-  public static void main(String[] arguments) {
-    int c; //declaring a variable
- 
-  /* Using for loop to repeat instruction execution */
- 
-    for (c = 1; c <= 10; c++) {
-      System.out.println(c);
-    }
-  }
-=======
->>>>>>> ac368511cc1563e5f47951f5efe43e6f02a77c2f
 }
